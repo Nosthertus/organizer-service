12d2b31fd590228f55e252eb2b815f5ceef4100b
nosthertus <nosthertus@gmail.com>
2017-06-22 22:33:54 -0500

    * Timezone fix
    *Fixed a glitch that was causing the server to provide a different timezone, although it is 100% accurate to guess what timezone the server is using so it tries its best to get the correct value

--- database/index.js
+++ database/index.js
@@ -1,11 +1,13 @@
 var Sequelize = require("sequelize");
 var flutils   = require("flutils");
+var moment    = require("moment-timezone");
 var debug     = require("debug")("app:database");
 var debugSQL  = require("debug")("app:database:SQL");
 var config    = flutils.loadJSON("config.json")[(process.env.ENV || "default")];
 
 module.exports = new Sequelize("organizer", config.database.user, config.database.password, {
 	dialect: "mysql",
+	timezone: moment.tz.guess(),
 	logging: (log) => {
 		debugSQL(log);
 	}
